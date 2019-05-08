b77386392e6e94b1d012ba056cd750cb93041f89
Pankaj Kumar Pramanik <pankaj.me2k9@gmail.com>
2019-05-08 13:42:17 +0600

    db file intiallize

---
+++ public/precache-manifest.02399ea2fc53957345601058b2cb0f99.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "59eb40275121817af2c0",
+    "url": "/static/css/main.655cdf89.chunk.css"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "474d23791e72925ff0a4",
+    "url": "/static/js/1.474d2379.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "59eb40275121817af2c0",
+    "url": "/static/js/main.59eb4027.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "474d23791e72925ff0a4",
+    "url": "/static/css/1.de001c81.chunk.css"
+  },
+  {
+    "revision": "abc9fe165ababefd28c73ff4efef68b5",
+    "url": "/index.html"
+  }
+];
